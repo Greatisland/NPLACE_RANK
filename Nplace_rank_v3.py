b1d7a531d5fe1eae26ac085a5b626a0f83a92040
Hyeonjin Kim <dontsendloveletters@gmail.com>
2024-12-23 19:50:49 +0900

    feat: 헤드리스 수정

--- Nplace_rank_v3.py
+++ Nplace_rank_v3.py
@@ -952,8 +952,11 @@ def get_naver_rank_public(keyword, 업체_ID):
     options = webdriver.ChromeOptions()
     options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
     options.add_argument('window-size=1380,900')
-    options.add_argument('headless=new')
+    # options.add_argument('headless=new')
+    options.add_argument('--headless')
     options.add_argument("disable-gpu")
+    options.add_argument('--no-sandbox')  
+    options.add_argument('--disable-dev-shm-usage') 
     options.add_experimental_option("prefs", {
         "profile.managed_default_content_settings.images": 2,       # 이미지 비활성화
         "profile.managed_default_content_settings.stylesheets": 2,  # CSS 비활성화
